1973f5ffc31f505426f1ecebb8be9db4e0c4be81
Michael Muraya <mikachupichu@gmail.com>
2024-01-24 00:38:48 +0300

    Back-end

--- server/app.py
+++ server/app.py
@@ -1,146 +1,260 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from models import db, User, Photo, Category, Comment, user_photo_interaction
+from models import db, User, Photo, Like
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photo-user.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'vsgewvwesvsgevafdsag'
 app.config['JWT_SECRET_KEY'] = 'vsgewvwesvsgevafdsag'
 
-db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
+db.init_app(app)
 jwt = JWTManager(app)
 
 # Routes
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    access_token = create_access_token(identity=new_user.id)
+    return jsonify(access_token=access_token), 200
 
-# User authentication
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
+    user = User.query.filter_by(username=data['username'], password=data['password']).first()
 
-    if user and user.check_password(data['password']):
-        access_token = create_access_token(identity=user.public_id)
+    if user:
+        access_token = create_access_token(identity=user.id)
         return jsonify(access_token=access_token), 200
     else:
-        return jsonify({"message": "Invalid username or password"}), 401
+        return jsonify(message="Invalid username or password"), 401
 
-# User registration
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
-    new_user.set_password(data['password'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"message": "User registered successfully"}), 201
 
-# Retrieve all users
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = User.query.all()
-    return jsonify([user.serialize() for user in users]), 200
+@app.route('/photos', methods=['GET'])
+def get_all_photos():
+    photos = Photo.query.all()
+    return jsonify(photos=[photo.serialize() for photo in photos]), 200
 
-# Retrieve a specific user
-@app.route('/user/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if user:
-        return jsonify(user.serialize()), 200
-    else:
-        return jsonify({"message": "User not found"}), 404
 
-# Update user information
-@app.route('/user/<int:user_id>', methods=['PUT'])
-@jwt_required()
-def update_user(user_id):
-    current_user = get_jwt_identity()
-    if current_user != user_id:
-        return jsonify({"message": "Unauthorized"}), 401
-
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"message": "User not found"}), 404
-
-    data = request.get_json()
-    user.username = data.get('username', user.username)
-    user.email = data.get('email', user.email)
-
-    db.session.commit()
-    return jsonify({"message": "User updated successfully"}), 200
-
-# Delete user
-@app.route('/user/<int:user_id>', methods=['DELETE'])
-@jwt_required()
-def delete_user(user_id):
-    current_user = get_jwt_identity()
-    if current_user != user_id:
-        return jsonify({"message": "Unauthorized"}), 401
-
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"message": "User not found"}), 404
-
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User deleted successfully"}), 200
-
-# Photo CRUD operations
-@app.route('/photos', methods=['GET', 'POST'])
-@jwt_required()
-def photos():
-    current_user = get_jwt_identity()
-
-    if request.method == 'GET':
-        photos = Photo.query.all()
-        return jsonify([photo.serialize() for photo in photos]), 200
-
-    elif request.method == 'POST':
-        data = request.get_json()
-        new_photo = Photo(name=data['name'], description=data['description'], price=data['price'], user_id=current_user)
-        db.session.add(new_photo)
-        db.session.commit()
-        return jsonify({"message": "Photo created successfully"}), 201
-
-# Like and unlike a photo
-@app.route('/like/<int:photo_id>', methods=['POST', 'DELETE'])
-@jwt_required()
-def like_unlike_photo(photo_id):
-    current_user = get_jwt_identity()
-    photo = Photo.query.get(photo_id)
-
-    if request.method == 'POST':
-        current_user = User.query.filter_by(public_id=current_user).first()
-        photo.liked_by.append(current_user)
-        db.session.commit()
-        return jsonify({"message": "Photo liked successfully"}), 200
-
-    elif request.method == 'DELETE':
-        current_user = User.query.filter_by(public_id=current_user).first()
-        photo.liked_by.remove(current_user)
-        db.session.commit()
-        return jsonify({"message": "Photo unliked successfully"}), 200
-
-# Display photo information
-@app.route('/photo/<int:photo_id>', methods=['GET'])
+@app.route('/photos/<int:photo_id>', methods=['GET'])
 def get_photo(photo_id):
     photo = Photo.query.get(photo_id)
-    return jsonify(photo.serialize()), 200
+    if photo:
+        return jsonify(photo.serialize()), 200
+    else:
+        return jsonify(message="Photo not found"), 404
 
-# Comment on a photo
-@app.route('/comment/<int:photo_id>', methods=['POST'])
+@app.route('/photos', methods=['POST'])
 @jwt_required()
-def add_comment(photo_id):
-    data = request.get_json()
-    current_user = get_jwt_identity()
-    new_comment = Comment(user_id=current_user, photo_id=photo_id, content=data['content'])
-    db.session.add(new_comment)
-    db.session.commit()
-    return jsonify({"message": "Comment added successfully"}), 201
+def create_photo():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        data = request.get_json()
+        new_photo = Photo(title=data['title'], description=data['description'], user=user)
+        db.session.add(new_photo)
+        db.session.commit()
+        return jsonify(message="Photo created successfully"), 201
+    else:
+        return jsonify(message="User not found"), 404
 
 
+@app.route('/photos/<int:photo_id>', methods=['DELETE'])
+@jwt_required()
+def delete_photo(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo and photo.user == user:
+            db.session.delete(photo)
+            db.session.commit()
+            return jsonify(message="Photo deleted successfully"), 200
+        else:
+            return jsonify(message="Photo not found or unauthorized"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+
+@app.route('/photos/<int:photo_id>', methods=['PUT'])
+@jwt_required()
+def update_photo(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo and photo.user == user:
+            data = request.get_json()
+            photo.title = data.get('title', photo.title)
+            photo.description = data.get('description', photo.description)
+            db.session.commit()
+            return jsonify(message="Photo updated successfully"), 200
+        else:
+            return jsonify(message="Photo not found or unauthorized"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+
+# Like/Unlike operations
+
+@app.route('/photos/<int:photo_id>/like', methods=['POST'])
+@jwt_required()
+def like_photo(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo:
+            like = Like.query.filter_by(user=user, photo=photo).first()
+            if not like:
+                new_like = Like(user=user, photo=photo)
+                db.session.add(new_like)
+                db.session.commit()
+                return jsonify(message="Photo liked successfully"), 201
+            else:
+                return jsonify(message="Photo already liked by the user"), 400
+        else:
+            return jsonify(message="Photo not found"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+
+@app.route('/photos/<int:photo_id>/like', methods=['DELETE'])
+@jwt_required()
+def unlike_photo(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo:
+            like = Like.query.filter_by(user=user, photo=photo).first()
+            if like:
+                db.session.delete(like)
+                db.session.commit()
+                return jsonify(message="Photo unliked successfully"), 200
+            else:
+                return jsonify(message="Photo not liked by the user"), 400
+        else:
+            return jsonify(message="Photo not found"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+# Get liked photos by a specific user
+
+@app.route('/user/<int:user_id>/liked-photos', methods=['GET'])
+@jwt_required()
+def get_liked_photos_by_user(user_id):
+    current_user_id = get_jwt_identity()
+    
+    if current_user_id == user_id:
+        user = User.query.get(user_id)
+        liked_photos = Photo.query.join(Like).filter(Like.user_id == user_id).all()
+        return jsonify(liked_photos=[photo.serialize() for photo in liked_photos]), 200
+    else:
+        return jsonify(message="Unauthorized access"), 401
+
+
+# Get extra photo information
+
+@app.route('/photos/<int:photo_id>/extra-info', methods=['GET'])
+def get_extra_photo_info(photo_id):
+    photo = Photo.query.get(photo_id)
+    
+    if photo:
+        return jsonify(extra_info={'creator': photo.user.username, 'description': photo.description}), 200
+    else:
+        return jsonify(message="Photo not found"), 404
+
+
+# Get all posts for a specific user account
+
+@app.route('/user/<int:user_id>/posts', methods=['GET'])
+@jwt_required()
+def get_user_posts(user_id):
+    current_user_id = get_jwt_identity()
+    
+    if current_user_id == user_id:
+        user = User.query.get(user_id)
+        user_posts = Photo.query.filter_by(user=user).all()
+        return jsonify(user_posts=[post.serialize() for post in user_posts]), 200
+    else:
+        return jsonify(message="Unauthorized access"), 401
+
+# Edit photo information
+
+@app.route('/photos/<int:photo_id>/edit', methods=['PUT'])
+@jwt_required()
+def edit_photo_info(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo and photo.user == user:
+            data = request.get_json()
+            photo.title = data.get('title', photo.title)
+            photo.description = data.get('description', photo.description)
+            db.session.commit()
+            return jsonify(message="Photo information updated successfully"), 200
+        else:
+            return jsonify(message="Photo not found or unauthorized"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+
+# Get all users
+
+@app.route('/users', methods=['GET'])
+@jwt_required()
+def get_all_users():
+    current_user_id = get_jwt_identity()
+    users = User.query.filter(User.id != current_user_id).all()
+    return jsonify(users=[user.serialize() for user in users]), 200
+
+
+# Delete liked photo
+
+@app.route('/photos/<int:photo_id>/delete-like', methods=['DELETE'])
+@jwt_required()
+def delete_like(photo_id):
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    if user:
+        photo = Photo.query.get(photo_id)
+        if photo:
+            like = Like.query.filter_by(user=user, photo=photo).first()
+            if like:
+                db.session.delete(like)
+                db.session.commit()
+                return jsonify(message="Like deleted successfully"), 200
+            else:
+                return jsonify(message="Like not found"), 404
+        else:
+            return jsonify(message="Photo not found"), 404
+    else:
+        return jsonify(message="User not found"), 404
+
+# @app.route('/users', methods=['GET'])
+# @jwt_required()
+# def get_all_users():
+#     users = User.query.all()
+#     return jsonify(users=[user.serialize() for user in users]), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,6 @@
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData, UniqueConstraint, ForeignKey, Table
 from flask_sqlalchemy import SQLAlchemy
-import bcrypt
-from werkzeug.security import generate_password_hash
-from flask_jwt_extended import create_access_token
-
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,99 +12,64 @@ db = SQLAlchemy(metadata=metadata)
 user_photo_interaction = Table(
     'user_photo_interaction',
     db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('photo_id', db.Integer, db.ForeignKey('photos.id'))
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('photo_id', db.Integer, db.ForeignKey('photo.id'))
 )
 
+# User model
 class User(db.Model):
-    __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String)
-    username = db.Column(db.String)
-    email = db.Column(db.String)
-    password_hash = db.Column(db.String)
-    profile_pic = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    photos = db.relationship('Photo', backref='user', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
 
-    photos = db.relationship('Photo', back_populates='user', cascade='all, delete-orphan')
-    comments = db.relationship('Comment', back_populates='user', cascade='all, delete-orphan')
-    liked_photos = db.relationship('Photo', secondary=user_photo_interaction, back_populates='liked_by')
-
-    __table_args__ = (UniqueConstraint('username', name='user_unique_constraint'),)
-
-    def __repr__(self):
-        return f'(id={self.id}, name={self.username} email={self.email} profile_pic={self.profile_pic})'
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
-
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
-
-    def generate_access_token(self, expires_in=604800):  # Token expires in 7 days by default
-        return create_access_token(identity=str(self.id), expires_delta=expires_in)
-
-   
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username
+            # Add other fields as needed
+        }
 
 
+# Photo model
 class Photo(db.Model):
-    __tablename__ = 'photos'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
-    price = db.Column(db.Numeric(precision=10, scale=2))
-    user_id = db.Column(db.Integer, ForeignKey('users.id'))
-    category_id = db.Column(db.Integer, ForeignKey('categories.id'))
-    image = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    likes = db.relationship('Like', backref='photo', lazy=True)
+    
+    # New fields
+    region = db.Column(db.String(50))
+    create_time = db.Column(db.DateTime)
+    size = db.Column(db.Integer)
+    image_url = db.Column(db.String(255))  # Assuming URL length is 255 characters
 
-    category = db.relationship('Category', back_populates='photos')
-    user = db.relationship('User', back_populates='photos')
-    comments = db.relationship('Comment', back_populates='photo', cascade='all, delete-orphan')
-    liked_by = db.relationship('User', secondary=user_photo_interaction, back_populates='liked_photos')
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'user_id': self.user_id,
+            'region': self.region,
+            'create_time': self.create_time,
+            'size': self.size,
+            'image_url': self.image_url
+            # Add other fields as needed
+        }
 
-    __table_args__ = (UniqueConstraint('name', name='img_name_unique_constraint'),)
 
-    def __repr__(self):
-        return f'(id={self.id}, name={self.name} description={self.description} price={self.price} price={self.image} user_id={self.user_id} category_id={self.category_id} )'
-
-
-class Comment(db.Model):
-    __tablename__ = 'comments'
-
+# Like model (many-to-many relationship between User and Photo)
+class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('users.id'))
-    photo_id = db.Column(db.Integer, ForeignKey('photos.id'))
-    content = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photo.id'), nullable=False)
 
-    user = db.relationship('User', back_populates='comments')
-    photo = db.relationship('Photo', back_populates='comments')
-
-    @validates('user_id')
-    def validate_user_id(self, key, user_id):
-        if self.photo.user_id != user_id:
-            raise ValueError("You can only add comments as the owner of the photo.")
-        return user_id
-
-    def __repr__(self):
-        return f'(id={self.id}, user_id={self.user_id}, photo_id={self.photo_id}, content={self.content})'
-
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    photos = db.relationship('Photo', back_populates='category', cascade='all, delete-orphan')
-
-    def __repr__(self):
-        return f'(id={self.id}, name={self.name})'
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'photo_id': self.photo_id
+            # Add other fields as needed
+        }
